3c8ec84536ade7d9dbcf6306edfbfcff53871e01
Shaw <shaw.creaney@signaturize.com>
2020-02-01 17:25:09 +0000

    HUD & Game Over

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUI : MonoBehaviour
+{
+    [SerializeField]
+    private ScriptableFloat health;
+
+    [SerializeField]
+    private RectTransform border;
+
+    [SerializeField]
+    private RectTransform bar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float width = border.rect.width;
+        float inset = (width / 100) * (100 - health);
+        float size = width - inset;
+        // bar.rect.Set(0, 0, width, bar.rect.height);
+        bar.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, inset, size);
+        // bar.sizeDelta = new Vector2 (width, bar.sizeDelta.y);
+    }
+}
