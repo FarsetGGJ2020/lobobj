50e1be56ef6f5f5a25b6a88de2184668caaaccb0
Stephan Williams <stephan.williams@signaturize.com>
2020-02-01 11:41:08 +0000

    Combine navmesh and controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,4 +6,15 @@ public class Player : Singleton<Player>
 {
     public int playerHealth;
     public string playerName;
+    [SerializeField] private ParticleSystem movementParticles;
+
+    public void StartMoving()
+    {
+        movementParticles.Play();   
+    }
+
+    public void StopMoving()
+    {
+        movementParticles.Stop();   
+    }
 }
